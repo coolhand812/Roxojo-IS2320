cb44be3ab17828c35aa0073b993efc893f4427de
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2019-02-21 22:54:55 -0600

    added biohazard pic to project; added security blurb and pic to page

---
+++ SU_MenuScreen.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>RUXOJO Project</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+		<div id="container">
+
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li>Add Student</li>
+                        <li>Student Lookup</li>
+                        <li>Monthly Faculty Report</li>
+                        <li>Annual Faculty Report</li>
+                        <li>Modify THIS account</li>
+                        <li>Modify User Account</li>
+					</ul>
+				</div>
+				
+				<div id="header2">
+					<h2>Administrator Menu</h2>
+				</div>
+				
+				<div id="main">
+
+                <img src="biohazard.png" alt="Biohazard Symbol" width="500" height="600">		
+				<p>	
+                You are accessing a UASLP Information System (IS) that is provided for UASLP-authorized use only.
+				</p>
+
+				</div>
+				
+			</div>
+			
+			<div id="footer">
+				Copyright &copy; 2019 RUXOJO
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
