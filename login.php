99ff9e932a55ab5347fdae08cac1908812c6c72b
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2019-03-03 22:17:56 -0600

    working on login errors

--- login.php
+++ login.php
@@ -4,7 +4,8 @@
     $userlvl='';        // Variable to store user level
     if (isset($_POST['submit'])) {      // Verifies submit was selected
         if (empty($_POST['username']) || empty($_POST['password'])) {   //checks fields are not empty
-            $error = "Username or Password is invalid";     // displays message if either field is empty
+            $error = "Username or Password is invalid";
+            echo $error;    // displays message if either field is empty     
         }
         else {
             // Define $username and $password
@@ -13,17 +14,25 @@
             
             // Establishing Connection with Server by passing server_name, user_id and password as a parameter
             $db = new mysqli("localhost", "root", "", "ruxojo_accountsreceivable");
-            
+
+            /* check connection */
+            if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+            }
+
             // SQL query to fetch information of registerd users and finds user match.
             // add MD5 to pswd
-            $stmt = $db->prepare("SELECT user_name FROM admin_table WHERE user_name= '$username' AND
-             p_word= '$password'");
-            $stmt->bind_param('ss', $username, $password); 
-            $stmt->execute();
-            $stmt->store_result();
-            
-            if ($stmt->num_rows == 0) {
+            $sql = "SELECT id FROM admin_table WHERE user_name= '$username' AND
+             p_word= '$password'";
+            //$userlvl=$_SESSION['user_level'];
+            $result = mysqli_query($db,$sql);
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+            $active = $row['active'];
+            $count = mysqli_num_rows($result);
+            if ($count == 0) {
                 $error = "Username or Password is invalid";
+                echo $error;
                 
             } else {
                 if($_SESSION['user_level'] == 1){
