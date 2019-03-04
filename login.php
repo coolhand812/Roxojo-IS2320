bc769d1de72eedba3f231f263ff238c6271131b4
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2019-03-03 20:28:37 -0600

    added login user level conditional

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
     session_start();    // Starting Session
     $error='';          // Variable To Store Error Message
+    $userlvl='';        // Variable to store user level
     if (isset($_POST['submit'])) {      // Verifies submit was selected
         if (empty($_POST['username']) || empty($_POST['password'])) {   //checks fields are not empty
             $error = "Username or Password is invalid";     // displays message if either field is empty
@@ -15,8 +16,8 @@
             
             // SQL query to fetch information of registerd users and finds user match.
             // add MD5 to pswd
-            $stmt = $db->prepare("SELECT username FROM admin_table WHERE username= '$username' AND
-             password= '$password'");
+            $stmt = $db->prepare("SELECT user_name FROM admin_table WHERE user_name= '$username' AND
+             p_word= '$password' AND user_level= '$userlvl'");
             $stmt->bind_param('ss', $username, $password); 
             $stmt->execute();
             $stmt->store_result();
@@ -25,9 +26,15 @@
                 $error = "Username or Password is invalid";
                 
             } else {
-                session_register("myusername");
-                $_SESSION['login_user']=$username; // Initializing Session
-                
+                if($_SESSION['user_level'] == 1){
+                    session_register("myusername");
+                    $_SESSION['login_user']=$username; // Initializing Session
+                    header("Location: SU_Menuscreen.html");
+                }else{
+                    session_register("myusername");
+                    $_SESSION['login_user']=$username; // Initializing Session
+                    header("Location: U_Menuscreen.html");
+                }
             }
             mysqli_close($db); // Closing Connection
         }
